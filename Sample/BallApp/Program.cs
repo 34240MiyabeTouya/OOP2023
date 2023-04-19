bc76916000c548da756f0e92f41fa13f8cb44b43
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-04-19 11:54:07 +0900

    4/19完成

--- Sample/BallApp/Program.cs
+++ Sample/BallApp/Program.cs
@@ -13,6 +13,10 @@ namespace BallApp {
         private SoccerBall soccerBall;
         private PictureBox pb;
 
+        private List<SoccerBall> balls = new List<SoccerBall>(); //ボールインスタンス格納用
+        private List<PictureBox> pbs = new List<PictureBox>();　//画像表示用
+
+
         static void Main(string[] args) {
             Application.Run(new Program());
         }
@@ -30,7 +34,6 @@ namespace BallApp {
 
         //マウスクリック時のイベントハンドラ
         private void Program_MouseClick(object sender, MouseEventArgs e) {
-            moveTimer.Start(); //タイマースタート
             //ボールインスタンス生成
             soccerBall = new SoccerBall(e.X-25,e.Y-25);
             pb = new PictureBox();
@@ -39,12 +42,20 @@ namespace BallApp {
             pb.Size = new Size(50, 50); //画像の表示サイズ
             pb.SizeMode = PictureBoxSizeMode.StretchImage; //画像の表示モード
             pb.Parent = this;
+
+            balls.Add(soccerBall);
+            pbs.Add(pb);
+
+            moveTimer.Start(); //タイマースタート
         }
 
         //タイマータイムアウト時のイベントハンドラ
         private void MoveTimer_Tick(object sender, EventArgs e) {
-            soccerBall.Move(); //移動
-            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY); //画像の位置
+            for (int i = 0; i < balls.Count; i++)
+            {
+                balls[i].Move();
+                pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY); //画像の位置
+            }
         }
     }
 }
