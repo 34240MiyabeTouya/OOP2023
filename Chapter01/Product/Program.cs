ac9bb945db3dadf3404345169d99d776cf6798f6
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-05-08 11:56:17 +0900

    0508演習１

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -7,11 +7,28 @@ using System.Threading.Tasks;
 namespace ProductSample {
 class Program {
         static void Main(string[] args) {
+            #region P26のサンプルプログラム
             //インスタンスの生成
-            Product karinto = new Product(123, "かりんとう", 180);
-            Product daifuku = new Product(235, "大福もち", 160);
-            Console.WriteLine("かりんとうの税込み価格" + karinto.GetPriceIncludingTax() + "円");
-            Console.WriteLine("大福もちの税込み価格" + daifuku.GetPriceIncludingTax() + "円");
+            //Product karinto = new Product(123, "かりんとう", 180);
+            //Product daifuku = new Product(235, "大福もち", 160);
+
+            //Console.WriteLine("かりんとうの税込み価格" + karinto.GetPriceIncludingTax() + "円");
+            //Console.WriteLine("大福もちの税込み価格" + daifuku.GetPriceIncludingTax() + "円");
+            #endregion
+            DateTime date = new DateTime(2023,5,8);
+
+            //現在
+            DateTime dateNaw = DateTime.Now;
+            Console.WriteLine(dateNaw.Day);
+
+            //10日後を求める
+            DateTime daysAfter10 = date.AddDays(10);
+            Console.WriteLine("今日の10日後は" + daysAfter10.Day + "日です。");
+
+
+            //10日前を求める
+            DateTime daysBffor = date.AddDays(-10);
+            Console.WriteLine("今日の10日前は"+daysBffor.Day + "日です。");
         }
     }
 }
