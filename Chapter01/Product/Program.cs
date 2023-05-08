6ea0b6bab1eed97182f6723b0b886faf85383c0a
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-05-08 10:56:39 +0900

    P26まで

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+class Program {
+        static void Main(string[] args) {
+            //インスタンスの生成
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product daifuku = new Product(235, "大福もち", 160);
+            Console.WriteLine("かりんとうの税込み価格" + karinto.GetPriceIncludingTax() + "円");
+            Console.WriteLine("大福もちの税込み価格" + daifuku.GetPriceIncludingTax() + "円");
+        }
+    }
+}
