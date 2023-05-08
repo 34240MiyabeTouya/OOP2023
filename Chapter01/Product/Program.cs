5133220fba6b02fd877d8ae54b5e47c9677d2b93
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-05-08 12:00:36 +0900

    0508演習１

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -15,20 +15,19 @@ class Program {
             //Console.WriteLine("かりんとうの税込み価格" + karinto.GetPriceIncludingTax() + "円");
             //Console.WriteLine("大福もちの税込み価格" + daifuku.GetPriceIncludingTax() + "円");
             #endregion
-            DateTime date = new DateTime(2023,5,8);
+            DateTime date = new DateTime(2023,5, 8);
 
             //現在
             DateTime dateNaw = DateTime.Now;
-            Console.WriteLine(dateNaw.Day);
+            Console.WriteLine("今日の日付は" + dateNaw.Day + "日です。");
 
             //10日後を求める
             DateTime daysAfter10 = date.AddDays(10);
             Console.WriteLine("今日の10日後は" + daysAfter10.Day + "日です。");
 
-
             //10日前を求める
-            DateTime daysBffor = date.AddDays(-10);
-            Console.WriteLine("今日の10日前は"+daysBffor.Day + "日です。");
+            DateTime daysBefore10 = date.AddDays(-10);
+            Console.WriteLine("今日の10日前は" + daysBefore10.Day + "日です。");
         }
     }
 }
