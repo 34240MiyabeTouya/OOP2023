57e0111257e7739790c12da63091d9b566346060
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-05-09 09:57:38 +0900

    演習２回答

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -16,22 +16,32 @@ class Program {
             //Console.WriteLine("大福もちの税込み価格" + daifuku.GetPriceIncludingTax() + "円");
             #endregion
             #region 演習１
-            DateTime date = new DateTime(2023, 5, 8);
+            //DateTime date = new DateTime(2023, 5, 8);
 
-            //現在
-            DateTime dateNaw = DateTime.Now;
-            Console.WriteLine("今日の日付:" + date.Year + "年" + date.Month + "月" + dateNaw.Day + "日です。");
+            ////現在
+            //DateTime dateNaw = DateTime.Now;
+            //Console.WriteLine("今日の日付:" + date.Year + "年" + date.Month + "月" + dateNaw.Day + "日です。");
 
-            //10日後を求める
-            DateTime daysAfter10 = date.AddDays(10);
-            Console.WriteLine("今日の10日後:" + daysAfter10.Year + "年" + daysAfter10.Month + "月" + daysAfter10.Day + "日です。");
+            ////10日後を求める
+            //DateTime daysAfter10 = date.AddDays(10);
+            //Console.WriteLine("今日の10日後:" + daysAfter10.Year + "年" + daysAfter10.Month + "月" + daysAfter10.Day + "日です。");
 
-            //10日前を求める
-            DateTime daysBefore10 = date.AddDays(-10);
-            Console.WriteLine("今日の10日前:" + daysBefore10.Year + "年" + daysBefore10.Month + "月" + daysBefore10.Day + "日です。");
+            ////10日前を求める
+            //DateTime daysBefore10 = date.AddDays(-10);
+            //Console.WriteLine("今日の10日前:" + daysBefore10.Year + "年" + daysBefore10.Month + "月" + daysBefore10.Day + "日です。");
             #endregion
 
-
+            Console.WriteLine("誕生日を入力");
+            Console.Write("西暦：");
+            int year = int.Parse(Console.ReadLine());
+            Console.Write("月：");
+            var month = int.Parse(Console.ReadLine());
+            Console.Write("日：");
+            var day = int.Parse(Console.ReadLine());
+            DateTime birth = new DateTime(year, month, day);
+            DateTime now = DateTime.Now;
+            TimeSpan timespan = now - birth;
+            Console.Write("あなたは生まれてから今日までで" + timespan.Days + "日目です");
         }
     }
 }
