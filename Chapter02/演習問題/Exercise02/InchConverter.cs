e4b0eccd6fcd19e35c5fb6c5feb318dcf0382851
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-05-17 12:09:43 +0900

    完成

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    //インチとメートルの単位変換クラス
+    public static　class InchConverter {
+        //定数
+        private const double retio = 0.0254;
+
+        //メートルからインチを求める
+        public static double FromMeter(double meter) {
+            return meter / retio;
+        }
+
+        //インチからメートルを求める
+        public static double ToMeter(double feet) {
+            return feet * retio;
+        }
+    }
+}
