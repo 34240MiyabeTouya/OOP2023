5800c5a6ac53e7727cb1b6a006bbca043b652bee
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-05-17 11:21:57 +0900

    p79問題2.1完成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Song {
+        //プロバティ
+        public string Title { get; set; }//歌のタイトル
+        public string ArtistName { get; set; } //アーティスト名
+        public int Length { get; set; } //演奏時間(秒)
+
+        //2.1.2
+        public Song(string title,string artistname,int length) {
+            Title = title;
+            ArtistName = artistname;
+            Length = length;
+        }
+
+    }
+}
