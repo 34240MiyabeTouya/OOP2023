5f87718fe106ec3219cd1987bb9a9ffcdc57b23d
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-05-22 09:58:47 +0900

    問題演習2.3完成

---
+++ Exercise03/Exercise03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+
+            var sales = new SalesCounter(@"date\sales.csv");
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore) {
+                Console.WriteLine("{0}{1:C}", obj.Key, obj.Value);
+                 
+            }
+        }
+    }
+}
---
+++ Exercise03/Exercise03/SalesCounter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class SalesCounter {
+
+        private IEnumerable<Sale> _sales;
+
+        //コンストラクタ
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
+        }
+
+        //店舗別売り上げを求める
+        public IDictionary<string, int> GetPerStoreSales() {
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _sales) {
+                if (dict.ContainsKey(sale.productCategory)) {
+                    dict[sale.productCategory] += sale.Amount;//店名が既に存在する
+                } else {
+                    dict[sale.productCategory] = sale.Amount;//店名が存在する
+                }
+            }
+            return dict;
+        }
+
+        //売り上げデータを読み込み、Saleオブジェクトのlistを返す
+        static List<Sale> ReadSales(string filePath) {
+            var sales = new List<Sale>();　//売り上げデータを格納する
+            var lines = File.ReadAllLines(filePath); //ファイルからすべてのデータを読み込む
+
+            foreach (string line in lines) {//すべての行から一行ずつ取り出す
+
+                var items = line.Split(',');　//区切りで項目別に分ける
+                //Saleインスタンスを生成
+                var sale = new Sale { ShopName = items[0], productCategory = items[1], Amount = int.Parse(items[2]) };
+                sales.Add(sale);　//Saleインスタンスをコレクションに追加
+            }
+            return sales;
+        }
+    }
+}
