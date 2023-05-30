3af8c0f46c214e7bb3b27e793ffe8b41f6b12ab6
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-05-30 11:53:23 +0900

    4.3 回答

--- Chapter04/演習問題/Exercise01/YearMonth.cs
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -10,7 +10,7 @@ namespace Exercise01 {
         public int Year { get; private set; }
         public int Month { get; private set; }
 
-        public YearMonth(int year,int month) {
+        public YearMonth(int year, int month) {
             Year = year;
             Month = month;
         }
@@ -18,18 +18,22 @@ namespace Exercise01 {
         //4.1.2
         public bool Is21Century {
             get {
-                return 2001 >= Year && Year <= 2100;
-             }
+                return 2001 <= Year && Year >= 2100;
+            }
         }
 
         //4.1.3
-        //public YearMonth AddOneMonth() {
-
-        //}
+        public YearMonth AddOneMonth() {
+            if (Month == 12) {
+                return new YearMonth(Year + 1, 1);
+            } else {
+                return new YearMonth(Year, Month + 1);
+            }
+        }
 
         //4.1.4
         //public override string ToString() {
-            
+
         //}
     }
 }
--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -1,19 +1,18 @@
-﻿using System;
+﻿using Exercise01;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Exercise01;
+
 
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
-            
-            
+
             var ym = YearMonth(2023, 5);
-
-
-
+            var c21 = ym.Is21Century;
+            var ymNextMonth = ym.AddOneMonth();
         }
     }
 }
