41150dbdea75f5b459582ae2cfaba9e22d2eb413
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-05-30 11:21:42 +0900

    4.2 完成

--- Chapter04/演習問題/Exercise01/YearMonth.cs
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -14,5 +14,22 @@ namespace Exercise01 {
             Year = year;
             Month = month;
         }
+
+        //4.1.2
+        public bool Is21Century {
+            get {
+                return 2001 >= Year && Year <= 2100;
+             }
+        }
+
+        //4.1.3
+        //public YearMonth AddOneMonth() {
+
+        //}
+
+        //4.1.4
+        //public override string ToString() {
+            
+        //}
     }
 }
--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Exercise01;
 
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
-
+            
+            
+            var ym = YearMonth(2023, 5);
 
 
 
