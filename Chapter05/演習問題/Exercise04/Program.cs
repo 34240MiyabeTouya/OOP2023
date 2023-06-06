716210e0f12ae568ae6e576c896a58a94d091d12
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-06-06 11:14:31 +0900

    5.4 回答

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,17 @@ namespace Exercise04 {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
 #elif StringArray
             string[] lines = {
-                 "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            }
-            
-#endif  
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+                "Novelist=太宰治;BestWork=人間失格;Born=1909",
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+            };
+
+
+
+#endif
+
+#if NonArray
             var nove = "Novelist=";
             var best = "BestWork=";
             var born = "Born=";
@@ -34,6 +42,35 @@ namespace Exercise04 {
             Console.WriteLine("作家　：" + nolist);
             Console.WriteLine("代表作　：" + bestWork);
             Console.WriteLine("誕生年　：" + Born);
+
         }
     }
+#elif StringArray
+            foreach (var line in lines) {
+                foreach (var pair in line.Split(';')) {
+                    var array = pair.Split('=');
+                    Console.WriteLine("{0}:{1}", ToJapanese(array[0]), array[1]);
+                }
+            }
+
+
+
+#endif
+
+            static string ToJapanese(string key) {
+                switch (key) {
+                    case "Novelist";
+                        return "作家";
+                    case "BestWork";
+                        return "代表作";
+                    case "Born";
+                        return "誕生年";
+                }
+                throw new ArgumentException("正しい引数ではありません");
+            }
+
+        }
+    }
+
 }
+
