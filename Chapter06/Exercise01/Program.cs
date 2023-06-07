50242cc71bb2e84266ef7075fd7d7b3314861bf5
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-06-07 10:53:43 +0900

    メモリの使用量

---
+++ Chapter06/Exercise01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 9, 2, 5, 7, 8, 9, 5, 2, };
+
+            var books = Books.GetBooks();
+            Console.WriteLine(numbers.Average());
+
+            var booksObj = books.Where(x => x.Title.Contains("物語")).Average(x=>x.Pages);
+
+            Console.WriteLine("平均{0}ページ", booksObj);
+
+            var longTitleBooks = books.OrderByDescending(x => x.Title.Length);
+            foreach (var book in longTitleBooks) {
+                Console.WriteLine("{0}{1}円",book.Title,book.Price);
+            }
+        }
+    }
+}
+
---
+++ Chapter06/Section02/Section02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = Enumerable.Range(1, 1000000).ToArray();
+            WriteTotalMemory("配列確保後");
+
+
+            //偶数だけ取り出す
+            var evenNumber = numbers.Where(n => n % 2 == 0).ToArray();
+
+            WriteTotalMemory("偶数抽出後");
+            //foreach (var num in evenNumber) {
+            //    Console.WriteLine("{0}",num);
+            //}
+
+
+            var ave = evenNumber.Average();
+            WriteTotalMemory("偶数抽出後平均");
+           
+
+
+        }
+        static void WriteTotalMemory(string header) {
+            var totalMemory = GC.GetTotalMemory(true) / 1024.0 / 1024.0;
+            Console.WriteLine($"{header}: {totalMemory:0.000 MB}");
+        }
+    }
+}
