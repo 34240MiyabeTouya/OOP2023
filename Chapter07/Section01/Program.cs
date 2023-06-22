5c5caae889553df212ed677eaab0bdecaaa2b6cc
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-06-22 11:54:45 +0900

    県庁所在地の登録　一覧表示まで完成

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -7,30 +7,48 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
-            var Dict = new Dictionary<string,string>();
+            var Dict = new Dictionary<string, string>();
 
             string pref;
             string city;
 
             Console.WriteLine("県庁所在地の登録");
             Console.Write("県名:");
-            
+
             while (true) {
                 pref = Console.ReadLine();
                 if (pref == "999") break;
+
                 Console.Write("所在地:");
                 city = Console.ReadLine();
-                Dict[pref] = city;
-                Console.Write("県名:");
-                
-            }
-            Console.Write("県名を入力:");
-            string ken = Console.ReadLine();
-            Console.WriteLine("{0}です。",Dict[ken]);
-            
-            
-            
+                //重複チェック
+                if (Dict.ContainsKey(pref)) {
+                    Console.Write("既に県名が登録されています。");
+                    Console.Write("上書きしますか(Y/N):");
 
+                    if (Console.ReadLine() == "Y") {
+                        Dict[pref] = city;//上書き処理
+                    }
+                } else {
+                    Dict[pref] = city;//登録処理
+                }
+                Console.Write("県名:");
+
+            }
+            Console.Write("1:一覧表示　2:県名指定　:");
+            if (Console.ReadLine() == "1") {
+                foreach (var item in Dict) {
+                    Console.WriteLine("{0}({1})", item.Key, item.Value);
+                }
+            } else if (Console.ReadLine() == "2") {
+                Console.Write("県名を入力");
+                string ken = Console.ReadLine();
+                Console.WriteLine("{0}です。", Dict[ken]);
+            }
+        }
+        class cityInfo {
+            string City { get; set; }
+            int Population { get; set; }
         }
     }
 }
