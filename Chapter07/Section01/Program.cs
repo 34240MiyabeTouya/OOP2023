407bcedae572b8ec6b7dda1a6aa3d8d99b4ad6e9
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-06-23 10:58:16 +0900

    CityInfoの追加　一覧表示　県名指定　完成

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -7,10 +7,11 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
-            var Dict = new Dictionary<string, string>();
+            var Dict = new Dictionary<string, CityInfo>();
 
             string pref;
             string city;
+            int population;
 
             Console.WriteLine("県庁所在地の登録");
             Console.Write("県名:");
@@ -21,34 +22,45 @@ namespace Section01 {
 
                 Console.Write("所在地:");
                 city = Console.ReadLine();
+
+                Console.Write("人口:");
+                population = int.Parse(Console.ReadLine());
+
                 //重複チェック
                 if (Dict.ContainsKey(pref)) {
                     Console.Write("既に県名が登録されています。");
                     Console.Write("上書きしますか(Y/N):");
 
                     if (Console.ReadLine() == "Y") {
-                        Dict[pref] = city;//上書き処理
+                        Dict[pref] = new CityInfo {//上書き処理
+                            City = city,
+                            Population = population,
+                        };
                     }
                 } else {
-                    Dict[pref] = city;//登録処理
+                    Dict[pref] = new CityInfo {//登録処理
+                        City = city,
+                        Population = population,
+                    };
                 }
                 Console.Write("県名:");
-
             }
+            Console.Write("");
             Console.Write("1:一覧表示　2:県名指定　:");
-            if (Console.ReadLine() == "1") {
+            var num = Console.ReadLine();
+            if (num == "1") {
                 foreach (var item in Dict) {
-                    Console.WriteLine("{0}({1})", item.Key, item.Value);
+                    Console.WriteLine("{0}【{1}({2})】", item.Key,item.Value.City,item.Value.Population);
                 }
-            } else if (Console.ReadLine() == "2") {
+            } else if (num == "2") {
                 Console.Write("県名を入力");
                 string ken = Console.ReadLine();
-                Console.WriteLine("{0}です。", Dict[ken]);
+                Console.WriteLine("【{0}({1})】です。", Dict[ken].City,Dict[ken].Population);
             }
         }
-        class cityInfo {
-            string City { get; set; }
-            int Population { get; set; }
-        }
+    }
+    class CityInfo {
+        public string City { get; set; }//都市
+        public int Population { get; set; } //人口
     }
 }
