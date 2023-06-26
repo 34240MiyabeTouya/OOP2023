ac403b7440c2aa0f29d8bb1214cb23d34a827b7c
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-06-26 15:33:10 +0900

    valueをlistに修正後　一覧表示、指定の表示完成

---
+++ Chapter07/Section02/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+    class Program {
+        static void Main(string[] args) {
+            var list = new List<string>() {
+
+            };
+            var Dict = new Dictionary<string, List<CityInfo>>();
+
+            string pref;
+            string city;
+            int population;
+
+
+            Console.WriteLine("都市の登録");
+            Console.Write("県名:");
+
+            while (true) {
+                pref = Console.ReadLine();
+                if (pref == "999") break;
+
+                Console.Write("都市:");
+                city = Console.ReadLine();
+
+                Console.Write("人口:");
+                population = int.Parse(Console.ReadLine());
+
+                //市町村インスタンスの生成
+                var cityinfo = new CityInfo {
+                    City = city,
+                    Population = population
+                };
+
+
+                if (Dict.ContainsKey(pref)) {
+                    //List<CityInfo>が登録済みのためaddでデータを追加
+                    Dict[pref].Add(cityinfo);
+
+                } else {
+                    //List<CityInfo>が存在しないため、Listを作成(new)してデータを登録
+                    Dict[pref] = new List<CityInfo> { cityinfo };
+                }
+                Console.Write("県名:");
+            }
+            Console.Write("1:一覧表示　2:県名指定　:");
+            var num = Console.ReadLine();
+            if (num == "1") {
+                foreach (var item in Dict) {
+                    foreach (var item1 in item.Value) {
+                        Console.WriteLine("{0}【{1}(人口:{2})】", item.Key, item1.City, item1.Population);
+                    }
+                }
+            } else if (num == "2") {
+                Console.Write("県名を入力");
+                string ken = Console.ReadLine();
+                foreach (var item in Dict[ken]) {
+                        Console.WriteLine("{0}【{1}(人口:{2})】", ken, item.City, item.Population);
+                }
+            }
+        }
+    }
+    class CityInfo {
+        public string City { get; set; }//都市
+        public int Population { get; set; } //人口
+    }
+}
+
