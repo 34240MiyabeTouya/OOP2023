efd276d6b430572867336a21444089023229e2ff
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-10-05 10:22:45 +0900

    Exercise02 完成

---
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var texts = new[] {
+               "Time is money.",
+               "What time is it?",
+               "It will take time.",
+               "We reorganized the timetable.",
+            };
+            foreach (var text in texts) {
+                var matches = Regex.Matches(text, @"time", RegexOptions.IgnoreCase);
+                foreach (var match in matches) {
+                    Console.WriteLine("{0}:{1}", text,);
+                }
+                
+            }
+            
+
+
+
+        }
+    }
+}
