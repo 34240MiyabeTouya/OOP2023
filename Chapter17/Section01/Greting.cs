37ead2dca9a1a20dd64ff54ebd3352b0e0185ed2
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-10-17 10:22:26 +0900

    継承を使ったポリモーフィズム

---
+++ Chapter17/Section01/Greting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class GretingMorning :Greetingbase{
+        public string GetMessage() {
+            return "おはよう";
+        }
+    }
+
+    class GreetingAfternoon :Greetingbase{
+        public string GetMessage() {
+            return "こんにちは";
+        }
+    }
+
+    class GreetingEvening :Greetingbase{
+        public string GetMessage() {
+            return "こんばんは";
+        }
+    }
+}
