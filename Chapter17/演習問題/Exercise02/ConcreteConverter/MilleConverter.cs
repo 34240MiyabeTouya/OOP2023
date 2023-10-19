5068d5167c1604e0cd532deb8defe1ab3a1531c4
宮部瞳也 <OIBC34240@ojs.ac.jp>
2023-10-19 12:08:23 +0900

    演習問題２まで解説済

---
+++ Chapter17/演習問題/Exercise02/ConcreteConverter/MilleConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02.ConcreteConverter {
+    public class MilleConverter : Framework.ConverterBase {
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "mille" || name == UnitName;
+        }
+        protected override double Ratio { get { return 1609.34; } }
+        public override string UnitName { get { return "マイル"; } }
+    }
+}
